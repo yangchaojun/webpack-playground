5729111a299c259dbadfd10187ed92ff526cfd9e
yangchaojun <prd-chaojun.yang@winwayworld.com>
2020-03-05 18:22:25 +0800

    add css-loader, style-loader, file-loadder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash'
 import variable from './variables'
+import './style.css'
+import Icon from './icon.png'
 
 function component() {
   const element = document.createElement('div')
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
+  const myIcon = new Image()
+  myIcon.src = Icon
+  element.appendChild(myIcon)
 
   return element
 }
