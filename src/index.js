508e10b672a571e2e3f4ea908d10494eb59fac3f
yangchaojun <prd-chaojun.yang@winwayworld.com>
2020-03-06 14:30:48 +0800

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash'
-
+import printMe from './print'
 function component() {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
 
   return element
 }
