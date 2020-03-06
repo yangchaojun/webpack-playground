19d35579ca547e71590861720bdcf38f72b3dc87
yangchaojun <prd-chaojun.yang@winwayworld.com>
2020-03-06 18:00:16 +0800

    Dynamic Imports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import _ from 'lodash'
-import printMe from './print'
-function component() {
-  const element = document.createElement('div')
-  const btn = document.createElement('button')
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ 'lodash')
+    .then(({ default: _ }) => {
+      const element = document.createElement('div')
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-  btn.innerHTML = 'Click me and check the console!'
-  btn.onclick = printMe
+      element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
-  element.appendChild(btn)
-
-  return element
+      return element
+    })
+    .catch(error => 'An error occurred while loading the component')
 }
 
-document.body.appendChild(component())
+getComponent().then(component => {
+  document.body.appendChild(component)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js',
-    another: './src/another-module.js'
+    app: './src/index.js'
   },
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
   },
   plugins: [
     new CleanWebpackPlugin(),
