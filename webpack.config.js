319ab2552ca9507d75ac4f8cc2c96a3a2513236f
yangchaojun <prd-chaojun.yang@winwayworld.com>
2020-03-06 17:01:50 +0800

    Development

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   plugins: [
     new CleanWebpackPlugin(),
