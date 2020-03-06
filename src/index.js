999a7fa907ced9bab271d10a989cb27fa71f1b05
yangchaojun <prd-chaojun.yang@winwayworld.com>
2020-03-06 14:08:46 +0800

    clean up

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import _ from 'lodash'
-import variable from './variables'
-import './style.css'
-import Icon from './icon.png'
 
 function component() {
   const element = document.createElement('div')
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-  element.classList.add('hello')
-  const myIcon = new Image()
-  myIcon.src = Icon
-  element.appendChild(myIcon)
 
   return element
 }
 
 document.body.appendChild(component())
-
-console.log(variable.a + variable.b)
